5ce3ec265e1aea0e0f8d1b7d9c5dc702dcb8244e
Vasiliy <Ukrainepochta2013@gmail.com>
2025-07-15 13:59:44 +0300

    cleared consol.logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css";
+import { useState } from "react";
+import { TodoItem } from "../components/TodoItem";
+import { AddTodo } from "../components/AddTodo";
 
 function App() {
-  return <></>;
+  const initialTodos = [
+    { id: 1, text: "lern react" },
+    { id: 2, text: "create todo-app" },
+    { id: 3, text: "to do deploy" },
+  ];
+
+  const [todos, setTodos] = useState(initialTodos);
+
+  const onAdd = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const onDelete = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>My Todo App</h1>
+        <AddTodo onAdd={onAdd} />
+        <div>
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} onDelete={onDelete} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
